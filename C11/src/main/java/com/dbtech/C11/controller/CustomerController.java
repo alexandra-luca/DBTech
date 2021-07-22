67e12ffc92d9ab841a52ce8a2a1acf0267e7b446
irina.toma <irina_toma@rocketmail.com>
2021-07-22 12:37:30 +0300

    get by id and add

--- C11/src/main/java/com/dbtech/C11/controller/CustomerController.java
+++ C11/src/main/java/com/dbtech/C11/controller/CustomerController.java
@@ -28,15 +28,47 @@ public class CustomerController {
         return customers;
     }
 
-//    @GetMapping("/customers/{id}")
-//    public Customer getCustomerById(@PathVariable(name = "id") Integer cid) {
-//
-//    }
-//
-//    @PutMapping("/customers")
-//    public Customer insertCustomers(@RequestBody Customer customer) {
-//
-//    }
+    @GetMapping("/customers/{id}")  //TODO return an exception, NOT 500(BETTER 404)
+    @ResponseBody
+    public Customer getCustomerById(@PathVariable(name = "id") Integer cid) {
+        Customer customer = this.jdbcTemplate.queryForObject("SELECT * FROM customers WHERE id="+cid,new CustomerRowMapper());
+        return customer;
+    }
+
+
+    @PutMapping("/customers")
+    @ResponseBody
+    public Boolean insertIntoCustomer(@RequestBody Customer customer){
+        String sqlQuery = "INSERT INTO `db_tech_school`.`customers`\n" +
+                "(`username`,\n" +
+                "`last_name`,\n" +
+                "`first_name`,\n" +
+                "`phone`,\n" +
+                "`address`,\n" +
+                "`city`,\n" +
+                "`postal_code`,\n" +
+                "`country`)\n" +
+                "VALUES\n" +
+                "(?,\n" +
+                "?,\n" +
+                "?,\n" +
+                "?,\n" +
+                "?,\n" +
+                "?,\n" +
+                "?,\n" +
+                "?\n" +
+                ");";
+
+        int n =this.jdbcTemplate.update(sqlQuery,customer.getUsername(),customer.getLastName(),
+                customer.getFirstName(),customer.getPhone(),customer.getAddress(),customer.getCity(),
+                customer.getPostalCode(),customer.getCountry());
+
+        if (n >0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 //
 //    @PutMapping("/customers/{id}")
 //    public Customer updateCustomerById(@PathVariable(name = "id") Integer cid, @RequestBody Customer customer) {
