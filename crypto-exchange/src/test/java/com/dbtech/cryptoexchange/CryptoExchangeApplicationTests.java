ef9a3f426bb99f764d81d91c7d2a6a6b9e7d0b0f
catalin <catalinstircu18@gmail.com>
2021-07-23 19:03:11 +0300

    customer login + password

---
+++ crypto-exchange/src/test/java/com/dbtech/cryptoexchange/CryptoExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbtech.cryptoexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
