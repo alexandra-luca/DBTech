3df395183c22a437e136b35d992dc76022b384cb
Gabi Gutu <gabi.gutu@gmail.com>
2021-07-27 13:28:52 +0300

    C13 added

---
+++ C13/src/main/java/com/dbtech/C13/C13Application.java
@@ -0,0 +1,13 @@
+package com.dbtech.C11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C13Application.class, args);
+	}
+
+}
---
+++ C13/src/test/java/com/dbtech/C11/C13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbtech.C11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
